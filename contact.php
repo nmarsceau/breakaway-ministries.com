22dd337e818b6ebd4821e510f5eef8ab9b0dfd2d
nmarsceau <nickmarsceau@gmail.com>
2020-06-28 21:47:39 -0400

    Remove unused Google Font; Remove old register page

--- contact.php
+++ contact.php
@@ -84,7 +84,6 @@ else {
 <head>
   <title>Contact | Breakaway Sport Ministries</title>
 
-  <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
   <link rel="stylesheet"
     href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
